822125afc1f0d2deb41681ea2211d98aa34e1a20
CrashAzure <112322730+CrashAzure@users.noreply.github.com>
2024-01-29 15:21:54 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.15 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+
+        while(n != 0)
+        {
+            if(n % 2 != 0)
+            {
+                answer++;
+                n--;
+            }
+            
+            n /=2;
+            
+        }
+        
+        
+        return answer;
+    }
+    
+    
+    
+}
