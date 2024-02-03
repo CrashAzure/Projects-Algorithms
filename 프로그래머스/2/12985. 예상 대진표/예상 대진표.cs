a7a716b30e496e43247238258036757496c8bc87
CrashAzure <112322730+CrashAzure@users.noreply.github.com>
2024-02-03 18:43:54 +0900

    [level 2] Title: 예상 대진표, Time: 0.16 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+
+        while(n > 0)
+        {
+            if(a - 1 / 2 == b - 1 / 2) return answer;
+            n /= 2;
+            a = (a - 1) / 2 + 1;
+            b = (b - 1) / 2 + 1;
+            answer++;
+            
+        }
+
+        return answer;
+    }
+}
