b678b9b8744f2034fa3f855dc9797c830832b472
CrashAzure <112322730+CrashAzure@users.noreply.github.com>
2024-02-14 16:36:25 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 15.85 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cs
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cs
@@ -3,29 +3,14 @@ using System.Collections.Generic;
 
 // 자신보다 크면서 가장 가까이 있는 뒷 수
 // 단순 2중 for 문 - 시간 초과
-// dp
-// ??
+// 끝 인덱스부터 스택에 담으면서 피킹
 public class Solution {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.Length];
         Stack<int> s = new Stack<int>();
         answer[numbers.Length - 1] = -1;
         s.Push(numbers[numbers.Length - 1]);
-        /*
-        for(int i = 0; i < numbers.Length - 1; i++)
-        {   
-             answer[i] = -1;
-            for(int j = i + 1; j < numbers.Length; j++)
-            {
-                if(numbers[i] < numbers[j]) 
-                {
-                    answer[i] = numbers[j];
-                    break;
-                }
-            }
-        }
-        */
-        
+
         for(int i = numbers.Length - 2; i >= 0; i--)
         {   
             int temp = -1;
@@ -41,16 +26,6 @@ public class Solution {
                     s.Pop();
             }
 
-            /*
-            foreach(var ll in l)
-            {
-                if(numbers[i] < ll) 
-                {
-                    answer[i] = ll;
-                    break;
-                }
-            }
-            */
             answer[i] = temp;
             s.Push(numbers[i]);
         }
