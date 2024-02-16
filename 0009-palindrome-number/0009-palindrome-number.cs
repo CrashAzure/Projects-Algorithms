5191b6f4ab03218f8479901d813081f9f41a9c24
CrashAzure <112322730+CrashAzure@users.noreply.github.com>
2024-02-16 18:46:33 +0900

    Delete 0009-palindrome-number directory

--- 0009-palindrome-number/0009-palindrome-number.cs
+++
@@ -1,9 +0,0 @@
-public class Solution {
-    public bool IsPalindrome(int x) {
-        string s = x.ToString();
-        char[] c = s.ToCharArray();
-        Array.Reverse(c);
-        string s1 = new string(c);
-        return s == s1;
-    }
-}
